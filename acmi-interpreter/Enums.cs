5d94cb5a467833f398413c387ee1d3a206d46f7d
Robin Lodholm <dlodholm@icloud.com>
2024-08-04 10:50:20 -0600

    Add Enums for Colors

---
+++ acmi-interpreter/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acmi_interpreter;
+
+public enum ACMIColor
+{
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Cyan,
+    Blue,
+    Violet
+}
