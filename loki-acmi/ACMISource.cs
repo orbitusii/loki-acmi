55d51100e0db9d515cb7c0f368b302d2b86a2c41
Robin Lodholm <dlodholm@icloud.com>
2024-08-07 17:13:18 -0600

    Add actual loki-acmi plugin project - This is the final library that'll get plugged into LOKI down the line

---
+++ loki-acmi/ACMISource.cs
@@ -0,0 +1,54 @@
+﻿using acmi_interpreter;
+using loki_bms_common.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using tacview_net;
+
+namespace loki_acmi;
+
+[XmlInclude(typeof(ACMISource))]
+public class ACMISource : LokiDataSource
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public TacviewNetworker Networker { get; set; } = new TacviewNetworker("hostname", 0);
+    private ACMIMission Mission { get; set; } = new ACMIMission();
+    private CancellationTokenSource cts = new CancellationTokenSource();
+
+    public override void Activate()
+    {
+        Networker.Start(Username, Password);
+        Task.Run(() => { UpdateMission(Networker, cts.Token)});
+    }
+
+    public override bool CheckAlive() => Networker.IsRunning;
+
+    private void UpdateMission(TacviewNetworker networker, CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            List<string> messages = new List<string>();
+            while(networker.QueuedMessages.TryDequeue(out string? result))
+                messages.Add(result);
+
+            Mission.UpdateWithData(messages.ToArray());
+            messages.Clear();
+        }
+    }
+
+    public override void Deactivate()
+    {
+        Networker.Stop();
+        cts.Cancel();
+    }
+
+    public override TrackDatum[] GetFreshData()
+    {
+
+    }
+}
---
+++ loki-acmi/LokiACMIPlugin.cs
@@ -0,0 +1,24 @@
+﻿global using loki_bms_common;
+using loki_acmi;
+using loki_bms_common.Plugins;
+
+[assembly: LokiPlugin(
+    RootType = typeof(LokiACMIPlugin),
+    PluginName = "LOKI-ACMI",
+    Description = "A plugin for LOKI that interfaces with Tacview Real Time Telementry",
+    Author = "Robin Lodholm",
+    DesiredLokiVersion = LokiVersion.Any,
+    Version = "0.1.0",
+    DataSourceTypes = new Type[] { typeof(ACMISource) },
+    CustomMenuTypes = new Type[] { }
+    )]
+
+namespace loki_acmi;
+
+public class LokiACMIPlugin : LokiPlugin
+{
+    public override void Init()
+    {
+        // Nothing to do here
+    }
+}
